7a59e0c4d2071571baa21807b9ff16954f0e8d24
Sem <sembrestels@riseup.net>
2012-05-07 05:23:10 +0200

    Added spanish language file.

---
+++ languages/es.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Elgg infinite scroll plugin language pack
+ *
+ * @package ElggInfiniteScroll
+ */
+
+$spanish = array(
+	'infinite_scroll:list_bottom' => "Has alcanzado el final de la lista, no hay más elementos a mostrar.",
+);
+
+add_translation("es", $spanish);
